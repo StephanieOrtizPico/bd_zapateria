3aa03668b9f1b49621781d4e8c29d18518f7d63d
StephanieOrtizPico <nicolleortiz@colegioguanenta.edu.co>
2024-09-30 18:10:26 -0500

    Se agregaron actualizaciones

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zapateria</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Zapateria La Sangileña</h1>
+    <div id="div inicio sesión">
+        <form action="modelo/loguear.php" method = "post">
+            <label for="">Usuario:</label>
+            <input type="text" name="usuario" id="" placeholder="Usuario" required>
+            <label for="">Contraseña:</label>
+            <input type="password" name="clave" id="" placeholder="Contraseña" required>
+            <button type="submit">Ingresar</button>
+        </form>
+    </div>
+</body>
+</html>
